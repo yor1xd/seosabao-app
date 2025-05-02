6a809bd3664e233cc34e47f428ae6788bf2d2b04
Gabriel Henrique <gabrielhenriquenf1@gmail.com>
2025-05-02 00:51:45 -0300

    add navbar
    non-functional, purely aesthetic

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,103 +1,53 @@
-import Image from "next/image";
+import Machines from "./components/machines";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <h1>
+        Máquinas em uso
+      </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex flex-row justify-evenly w-full sm:h-40 p-5 gap-x-2">
+        <div className="justify-center relative border-2 rounded-2xl border-[rgba(44,150,80,0.25)] aspect-square sm:max-w-1/4 w-1/5 sm:flex-grow">
+            <div className="absolute w-full h-full hidden sm:flex flex-col justify-evenly p-5">
+              <p className="text-xl text-black font-extralight">Livre</p>
+              <p className="text-3xl text-black font-bold">2</p>
+
+            </div>
+          <p className="absolute top-1/2 left-1/2 -translate-1/2 text-3xl text-black font-bold sm:hidden">1</p>
+          <p className="absolute bottom-full left-1/2 -translate-x-1/2 text-md font-extralight sm:hidden">Livre</p>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+        <div className="justify-center relative border-2 rounded-2xl border-[rgba(44,150,80,0.25)] aspect-square sm:max-w-1/4 w-1/5 sm:flex-grow">
+            <div className="absolute w-full h-full hidden sm:flex flex-col justify-evenly p-5">
+              <p className="text-xl text-black font-extralight">Em uso</p>
+              <p className="text-3xl text-black font-bold">2</p>
+
+            </div>
+          <p className="absolute top-1/2 left-1/2 -translate-1/2 text-3xl text-black font-bold sm:hidden">1</p>
+          <p className="absolute bottom-full left-1/2 -translate-x-1/2 text-md font-extralight sm:hidden">Livre</p>
+
+        </div>
+
+
+        <div className="justify-center relative border-2 rounded-2xl border-[rgba(44,150,80,0.25)] aspect-square sm:max-w-1/4 w-1/5 sm:flex-grow">
+            <div className="absolute w-full h-full hidden sm:flex flex-col justify-evenly p-5">
+              <p className="text-xl text-black font-extralight">Em espera</p>
+              <p className="text-3xl text-black font-bold">2</p>
+
+            </div>
+          <p className="absolute top-1/2 left-1/2 -translate-1/2 text-3xl text-black font-bold sm:hidden">1</p>
+          <p className="absolute bottom-full left-1/2 -translate-x-1/2 text-md font-extralight sm:hidden">Livre</p>
+
+        </div>
+
+      </div>
+
+      <Machines/>
+
     </div>
   );
 }
