b4868c0cc51a7d651a7d1f4112c63f2e4853a3a6
Gabriel Henrique <gabrielhenriquenf1@gmail.com>
2025-05-03 17:09:34 -0300

    small things

--- frontend/app/components/progressbar.tsx
+++ frontend/app/components/progressbar.tsx
@@ -3,18 +3,16 @@
 import { useState } from "react";
 
 const ProgressBar = () => {
-    const [progress, setProgress] = useState(0);
-
     return (
         <div className="relative flex flex-col justify-evenly gap-y-2">
             <p>Progresso atual</p>
             <div className="w-full h-[20px] rounded-2xl bg-[#E8F2E8] overflow-hidden">
-                <div className="h-full bg-[#1AE51A] rounded-2xl transition-all duration-300" style={{width: `${progress}%`}}></div>
+                <div className="h-full bg-[#1AE51A] rounded-2xl transition-all duration-300" style={{width: `${0}%`}}></div>
 
             </div>
-            <p><span>Tempo estimado: {progress}min</span></p>
+            <p><span>Tempo estimado: {0}min</span></p>
 
-            <p className="absolute top-0 right-0">{progress}%</p>
+            <p className="absolute top-0 right-0">{0}%</p>
 
 
         </div>
